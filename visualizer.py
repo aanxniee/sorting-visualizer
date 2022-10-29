0989262286a73a866c1a556572989b21d289da58
Annie Cai <caishajia@gmail.com>
2022-10-29 18:21:56 -0400

    moved part of the initalization into a class

--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,29 @@
 import pygame, random, math
-
 pygame.init()
 
-screenLength, screenHeight = 600, 300;
-win = pygame.display.set_mode((screenLength, screenHeight)) 
-pygame.display.set_caption("Sorting Algorithm Visualizer")
+class base:
+    FONT = pygame.font.SysFont('Poppins', 20)
+    SIDE_PADDING = 120
+    TOP_PADDING = 50
+
+    def __init__(self, w, h, arr):
+        self.width = w
+        self.height = h
+        self.window = pygame.display.set_mode((w, h)) 
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+        self.set_arr(arr)
+
+    def set_list(self, arr):
+        self.arr = arr
+        self.minVal = min(arr)
+        self.maxVal = max(arr)
+
+        self.barWidth = round(self.width - self.SIDE_PADDING) / (2 * len(arr))
+        self.barHeight = math.floor((self.height - self.TOP_PADDING) / (self.maxVal - self.minVal))
+        self.startingX = self.SIDE_PADDING // 2
+
 
 clock = pygame.time.Clock()
-
 
 def generateArray(n, minVal, maxVal):
     arr = []
