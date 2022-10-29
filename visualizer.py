35d132612daa767f272bbc4e227779a3f79461ae
Annie Cai <caishajia@gmail.com>
2022-10-29 18:09:21 -0400

    generate array for sorting

--- visualizer.py
+++ visualizer.py
@@ -8,8 +8,24 @@ pygame.display.set_caption("Sorting Algorithm Visualizer")
 
 clock = pygame.time.Clock()
 
+
+def generateArray(n, minVal, maxVal):
+    arr = []
+
+    for i in range(n):
+        arr.append(random.randint(minVal, maxVal))
+
+    return arr
+
 # ------- MAIN GAME LOOP -------
 run = True
+n = 20
+minVal, maxVal = 0, 100
+
+sorting = False
+ascending = True
+
+arr = generateArray(n, minVal, maxVal)
 
 while run:
     win.fill((0,0,0))
